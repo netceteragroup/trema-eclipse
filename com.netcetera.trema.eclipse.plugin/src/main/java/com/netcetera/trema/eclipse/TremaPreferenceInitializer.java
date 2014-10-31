60004a3c533e6ee6d14a9da6dabe8e615bfd1abb
Jacques Stadler <jacques.stadler@netcetera.com>
2014-10-31 12:22:35 +0100

    Replaced use of deprecated constructor

--- com.netcetera.trema.eclipse.plugin/src/main/java/com/netcetera/trema/eclipse/TremaPreferenceInitializer.java
+++ com.netcetera.trema.eclipse.plugin/src/main/java/com/netcetera/trema/eclipse/TremaPreferenceInitializer.java
@@ -10,9 +10,7 @@ public class TremaPreferenceInitializer extends AbstractPreferenceInitializer {
 
   @Override
   public void initializeDefaultPreferences() {
-    // FIXME remove deprecated constructor when switching to target plattform
-    // 3.7
-    IEclipsePreferences node = new DefaultScope().getNode("com.netcetera.trema.eclipse");
+    IEclipsePreferences node = DefaultScope.INSTANCE.getNode("com.netcetera.trema.eclipse");
     node.put(TremaPreferencePage.MASTERVALUECONFLICT_RADIO_ID, TremaPreferencePage.MASTERVALUECONFLICT_RADIO_OPTION_FILE);
   }
 
